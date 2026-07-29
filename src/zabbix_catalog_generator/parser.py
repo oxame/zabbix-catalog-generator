6c44b30ec39ff27da1cf08cec8de9695734daacd
oxame <37661797+oxame@users.noreply.github.com>
2026-07-29 08:08:22 +0200

    fix: import Iterable from collections.abc

--- src/zabbix_catalog_generator/parser.py
+++ src/zabbix_catalog_generator/parser.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
+from collections.abc import Iterable
 from dataclasses import replace
 from pathlib import Path
-from typing import Any, Iterable
+from typing import Any
 
 import yaml
 
